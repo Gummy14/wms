c8fbd6f0b05ee4b556372a6969441c0bf729c73e
Alex Helm <alex.helm@yahoo.com>
2024-09-27 17:18:51 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Home/>
+  </div>
+</template>
+
+<script setup>
+import Home from './components/Home.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-text-field label="Item Name" v-model="itemData.name"></v-text-field>
+    <v-text-field label="Item Description" v-model="itemData.description"></v-text-field>
+    <v-btn @click="addDataToDatabase">Submit</v-btn>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      itemData: {
+        name: '',
+        description: ''
+      }
+    }
+  },
+  methods: {
+    addDataToDatabase() {
+      axios.post('https://localhost:7187/Item/AddItem', this.itemData)
+      .then(response => console.log(response))
+    }
+  },
+  mounted() {
+    axios.get('https://localhost:7187/Item/GetAllItems')
+    .then(response => console.log(response))
+  }
+}
+</script>
+
+<style scoped>
+</style>
