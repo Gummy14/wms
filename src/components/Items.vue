ecc9152286ee90026619bf21affbf89815a8f68d
Gummy14 <alex.helm@yahoo.com>
2024-12-01 19:55:42 -0500

    Can Now Read Printed QR Codes And Register Items From Them

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -8,12 +8,28 @@
         <v-text-field label="Item Name" v-model="itemToRegister.name"></v-text-field>
         <v-text-field label="Item Description" v-model="itemToRegister.description"></v-text-field>
       </v-card>
-      <v-btn @click="registerItem()">Register Item</v-btn>
+      <v-btn @click="printItemQrCode()">Print Item QR Code</v-btn>
+
+      <QrcodeStream @detect="onDetect" />
+
       <v-card>
         <v-list>
           <v-list-item v-for="item in allItems" :key="item.itemId" :title="item.name"></v-list-item>
         </v-list>
       </v-card>
+
+      <v-dialog
+        v-model="scannedQrCode"
+        width="auto"
+      >
+        <v-card v-if="scannedQrCode"
+          max-width="400"
+          :text="scannedQrCode.Description"
+          :title="scannedQrCode.Name"
+        >
+          <v-btn @click="registerItem()">Register Item</v-btn>
+        </v-card>
+      </v-dialog>
     </div>
   </div>
 </template>
@@ -21,26 +37,32 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import { GetAllItems, RegisterItem, PrintItemQRCode } from '@/functions/functions'
+import { QrcodeStream } from 'vue-qrcode-reader'
 
 var allItems = ref(null)
 var retrievedAllItems = ref(false)
+var scannedQrCode = ref(null)
 var itemToRegister = ref({
   itemId: null,
   name: '',
   description: ''
 })
 
-function registerItem() {
-  console.log('item', JSON.stringify(itemToRegister.value))
+function printItemQrCode() {
   PrintItemQRCode(itemToRegister.value)
   .then(() => {
     console.log('saved')
   })
-  // RegisterItem(itemToRegister.value)
-  // .then(() => {
-  //   itemToRegister.value.name = ''
-  //   itemToRegister.value.description = ''
-  // })
+}
+function registerItem() {
+  RegisterItem(scannedQrCode.value)
+  .then(() => {
+    scannedQrCode.value = null
+  })
+}
+function onDetect(detectedCodes) {
+  scannedQrCode.value = JSON.parse(detectedCodes[0].rawValue)
+  console.log('scannedQrCode.value.name', scannedQrCode.value.Name)
 }
 
 onMounted(() => {
