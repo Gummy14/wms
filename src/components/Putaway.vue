03514225e67b32ce82441893fbbded90e155ca61
Gummy14 <alex.helm@yahoo.com>
2024-12-18 18:36:40 -0500

    Putaway Scanning Process Updated

--- src/components/Putaway.vue
+++ src/components/Putaway.vue
@@ -13,15 +13,16 @@
           :text="putawayItem.description"
           :title="putawayItem.name"
         >
-        <div v-if="putawayContainer == null">
+        <div v-if="putawayLocation == null">
           <v-btn @click="selectForPutaway()">Select For Putaway</v-btn>
         </div>
         <div v-else>
-          Putaway In Location: {{ putawayContainer.name }}
-          <v-btn @click="setContainerFull()">Is Putaway Container Full? Request New Container</v-btn>
-          <Scanner must-verify @codeScanned="(emittedData) => scannedContainer = emittedData" />
-          <div v-if="containerIdDoesNotMatch">Incorrect Container Scanned</div>
-          <v-btn v-if="scannedContainer != null" @click="putItemInContainer()">Matching Container Successfully Scanned. Confirm Putaway</v-btn>
+          Putaway In Location: {{ putawayLocation.name }}
+          <!-- <v-btn @click="setContainerFull()">Is Putaway Container Full? Request New Container</v-btn> -->
+          <Scanner must-verify @codeScanned="(emittedData) => setScannedData(emittedData)" />
+          <div v-if="scannedPutawayLocation">Location Scanned</div>
+          <div v-if="scannedPutawayObject">Object Scanned</div>
+          <v-btn v-if="scannedPutawayLocation && scannedPutawayObject" @click="putItemInContainer()">Confirm Putaway</v-btn>
         </div>
         </v-card>
       </v-dialog>
@@ -34,10 +35,10 @@ import { ref } from 'vue'
 import { UpdateWarehouseObject, GetPutawayLocation } from '@/functions/functions'
 import Scanner from '@/components/scanning/Scanner.vue'
 
-var putawayContainer = ref(null)
-var scannedContainer = ref(null)
+var putawayLocation = ref(null)
 var putawayItem = ref(null)
-var containerIdDoesNotMatch = ref(null)
+var scannedPutawayLocation = ref(null)
+var scannedPutawayObject = ref(null)
 
 function selectForPutaway() {
   if(putawayItem.value.objectType == 0) {
@@ -51,24 +52,34 @@ function selectForPutaway() {
 function getPutawayLocation() {
   GetPutawayLocation()
   .then(response => {
-    putawayContainer.value = response.data
+    putawayLocation.value = response.data
   })
 }
+function setScannedData(scannedData) {
+  console.log('YES')
+  if (scannedData.objectType == 1 && scannedPutawayLocation.value == null) {
+    console.log('HERE')
+    scannedPutawayLocation.value = scannedData
+  } else if (scannedData.objectType == 0 && scannedPutawayLocation.value != null && scannedPutawayObject.value == null) {
+    console.log('HERE2')
+    scannedPutawayObject.value = scannedData
+  }
+}
 // function setContainerFull() {
-//   putawayContainer.value.isFull = true
-//   putawayContainer.value.eventType = 111
-//   UpdateContainerDetail(putawayContainer.value)
+//   putawayLocation.value.isFull = true
+//   putawayLocation.value.eventType = 111
+//   UpdateContainerDetail(putawayLocation.value)
 //   .then(() => {
 //     getPutawayLocation()
 //   })
 // }
 function putItemInContainer() {
-  putawayItem.value.parentId = putawayContainer.value.objectId
+  putawayItem.value.parentId = putawayLocation.value.objectId
   putawayItem.value.eventType = 310
   UpdateWarehouseObject(putawayItem.value)
   .then(() => {
     putawayItem.value = '',
-    putawayContainer.value = null,
+    putawayLocation.value = null,
     putawayItem.value = null
   })
 }
