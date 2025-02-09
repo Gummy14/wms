c8031cab8ee0b50b3c860eb25762936e8130894e
Gummy14 <alex.helm@yahoo.com>
2025-02-09 01:36:51 -0500

    Adding Details On Items List

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -5,9 +5,53 @@
     </div>
     <div v-else>
       <v-card>
-        <v-list>
-          <v-list-item v-for="item in allItems" :key="item.itemId" :title="item.name"></v-list-item>
-        </v-list>
+        <v-expansion-panels multiple>
+          <v-expansion-panel v-for="item in allItems">
+            <v-expansion-panel-title v-slot="{ expanded }">
+             <v-row no-gutters>
+               <v-col>
+                 {{ item.name }}
+               </v-col>
+             </v-row>
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <v-row>
+                <v-col>Description:</v-col>
+                <v-col>{{ item.description }}</v-col>
+              </v-row>
+              <v-divider class="border-opacity-25"></v-divider>
+              <v-row>
+                <v-col>Item ID:</v-col>
+                <v-col>{{ item.id }}</v-col>
+              </v-row>
+              <v-divider class="border-opacity-25"></v-divider>
+              <v-row>
+                <v-col>Location:</v-col>
+                <v-col>{{ item.locationId }}</v-col>
+              </v-row>
+              <v-divider class="border-opacity-25"></v-divider>
+              <v-row>
+                <v-col>Container:</v-col>
+                <v-col>{{ item.containerId }}</v-col>
+              </v-row>
+              <v-divider class="border-opacity-25"></v-divider>
+              <v-row>
+                <v-col>Order:</v-col>
+                <v-col>{{ item.orderId }}</v-col>
+              </v-row>
+              <v-divider class="border-opacity-25"></v-divider>
+              <v-row>
+                <v-col>Status:</v-col>
+                <v-col>{{ item.status }}</v-col>
+              </v-row>
+              <v-divider class="border-opacity-25"></v-divider>
+              <v-row>
+                <v-col>Timestamp Of Last Status Change:</v-col>
+                <v-col>{{ item.dateTimeStamp }}</v-col>
+              </v-row>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </v-card>
     </div>
   </div>
@@ -15,7 +59,7 @@
 
 <script setup>
 import { onMounted, ref } from 'vue'
-import { GetAllItems } from '@/functions/functions'
+import { GetAllItems, GetLocationById, GetContainerById, GetOrderById } from '@/functions/functions'
 
 var allItems = ref(null)
 var retrievedAllItems = ref(false)
