3448f589aa9f38aa7ef36b4c8d07b046d32ffbc9
Gummy14 <alex.helm@yahoo.com>
2025-04-03 14:34:16 -0400

    Adding Table setup

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,78 +1,67 @@
 <template>
-  <div>
-    <v-btn
-      @click="openScanner = true"
-      :prepend-icon="mdiQrcodeScan"
-    >
-      <template v-slot:prepend>
-        <svg-icon type="mdi" :path="mdiQrcodeScan"></svg-icon>
-      </template>
-      Scanner
-    </v-btn>
+  <v-app>
+    <v-container>
+      <v-select 
+        label="Warehouse Objects"
+        item-title="type"
+        item-value="id"
+        :items="warehouseObjectList"
+        v-model="warehouseObjectListSelection"
+      >
+      </v-select>
+      <div v-if="warehouseObjectListSelection == 0">
+        <Items />
+      </div>
+      <div v-else-if="warehouseObjectListSelection == 1">
+        <Locations />
+      </div>
+      <div v-else-if="warehouseObjectListSelection == 2">
+        <Containers />
+      </div>
+      <div v-else-if="warehouseObjectListSelection == 3">
+        <Orders />
+      </div>
+      <v-dialog 
+        v-model="openRegistration"
+        max-width="400"
+      >
+        <Registration />
+      </v-dialog>
+      <v-dialog 
+        v-model="openAddition"
+        max-width="400"
+      >
+        <Addition />
+      </v-dialog>
+      
+      <v-bottom-navigation>
+        <v-bottom-sheet>
+          <template v-slot:activator="{ props: activatorProps }">
+            <v-btn v-bind="activatorProps" text="Open Tools"></v-btn>
+          </template>
+        
+          <v-card
+            title="Tools"
+            text="Warehouse Operation Tools"
+          >
+            <ObjectScanner v-if="openScanner" />
 
-    <v-btn
-      @click="openRegistration = true"
-      :prepend-icon="mdiPlus"
-    >
-      <template v-slot:prepend>
-        <svg-icon type="mdi" :path="mdiPlus"></svg-icon>
-      </template>
-      Register New Warehouse Object
-    </v-btn>
+            <v-btn
+              v-for="tool in tools"
+              @click="openScanner = true"
+              :prepend-icon="mdiQrcodeScan"
+              :text="tool[0]"
+            >
+              <template v-slot:prepend>
+                <svg-icon type="mdi" :path="tool[1]"></svg-icon>
+              </template>
+            </v-btn>
 
-    <v-btn
-      @click="openAddition = true"
-      :prepend-icon="mdiPlus"
-    >
-      <template v-slot:prepend>
-        <svg-icon type="mdi" :path="mdiPlus"></svg-icon>
-      </template>
-      Add Previously Printed QR Code
-    </v-btn>
-
-    <v-select 
-      label="Warehouse Objects"
-      item-title="type"
-      item-value="id"
-      :items="warehouseObjectList"
-      v-model="warehouseObjectListSelection"
-    >
-    </v-select>
-
-    <div v-if="warehouseObjectListSelection == 0">
-      <Items />
-    </div>
-    <div v-else-if="warehouseObjectListSelection == 1">
-      <Locations />
-    </div>
-    <div v-else-if="warehouseObjectListSelection == 2">
-      <Containers />
-    </div>
-    <div v-else-if="warehouseObjectListSelection == 3">
-      <Orders />
-    </div>
-
-    <v-dialog 
-      v-model="openScanner"
-      max-width="400"
-    >
-      <ObjectScanner />
-    </v-dialog>
-
-    <v-dialog 
-      v-model="openRegistration"
-      max-width="400"
-    >
-      <Registration />
-    </v-dialog>
-
-    <v-dialog 
-      v-model="openAddition"
-      max-width="400"
-    >
-      <Addition />
-    </v-dialog>
-  </div>
+          </v-card>
+        </v-bottom-sheet>
+      </v-bottom-navigation>
+    </v-container>
+  </v-app>
 </template>
 
 <script setup>
@@ -92,6 +81,11 @@ var warehouseObjectList = ref([{id: 0, type: 'Items'}, {id: 1, type: 'Locations'
 var openScanner = ref(false)
 var openRegistration = ref(false)
 var openAddition = ref(false)
+var tools = ref([
+  ["Scanner", mdiQrcodeScan],
+  ["Register New Warehouse Object", mdiPlus],
+  ["Add Previously Printed QR Code", mdiPlus]
+])
 
 </script>
 
--- src/components/home/Items.vue
+++ src/components/home/Items.vue
@@ -1,20 +1,38 @@
 <template>
-  <div>
-    <div v-if="!retrievedAllItems">
-      <v-progress-circular indeterminate></v-progress-circular>
-    </div>
-    <div v-else>
-      <v-card>
-        <ItemList :items="allItems" />
-      </v-card>
-    </div>
-  </div>
+  <v-app>
+    <v-container>
+      <div v-if="!retrievedAllItems">
+        <v-progress-circular indeterminate></v-progress-circular>
+      </div>
+      <div v-else>
+        <v-card>
+          <v-data-table 
+            :items="allItems"
+            :headers="headers"
+          >
+          </v-data-table>
+          <!-- <ItemList :items="allItems" /> -->
+        </v-card>
+      </div>
+    </v-container>
+  </v-app>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
 import { GetAllItems } from '@/functions/functions'
-import ItemList from '@/components/lists/ItemList.vue'
+//import ItemList from '@/components/lists/ItemList.vue'
+
+const headers = [
+    { title: 'ID', align: 'start', key: 'id' },
+    { title: 'Name', align: 'start', key: 'name' },
+    { title: 'Description', align: 'start', key: 'description' },
+    { title: 'Status', align: 'start', key: 'status' },
+    { title: 'DateTime Last Modified', align: 'start', key: 'dateTimeStamp' },
+    { title: 'Location Name', align: 'start', key: 'locationName' },
+    { title: 'Container Name', align: 'start', key: 'containerName' },
+    { title: 'Order Name', align: 'start', key: 'orderName' },
+  ]
 
 var allItems = ref(null)
 var retrievedAllItems = ref(false)
